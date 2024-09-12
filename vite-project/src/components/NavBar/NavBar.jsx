5f281e7415feffacc359e0c9bcb7cec5d0c527be
tomimoure8 <tomimoure8@gmail.com>
2024-09-11 22:08:36 -0300

    actualización final

---
+++ vite-project/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Navbar () {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/about">Sobre Nosotros</a></li>
+                <li><a href="/contact">Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
